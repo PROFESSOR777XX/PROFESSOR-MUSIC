fc2c38b9ed6178da62e895281816839574803495
PROFESSOR777XX <professor7711x@gmail.com>
2024-08-28 09:53:24 +0530

    Update start.py

--- PURVIMUSIC/utils/inline/start.py
+++ PURVIMUSIC/utils/inline/start.py
@@ -21,8 +21,9 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true"),
         ],
-        [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+        text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
